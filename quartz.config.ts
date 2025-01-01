7aa8d7eeb8a3aa1e85824c11f04bf9fe9c280b94
Kaede Sato <214kaedesato@gmail.com>
2025-01-01 23:31:42 +0900

    created - filesystem only

--- quartz.config.ts
+++ quartz.config.ts
@@ -58,7 +58,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
+        priority: ["filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
