d93c7d078cce657c10e1fdec38edea7de44c9414
Kaede Sato <214kaedesato@gmail.com>
2025-03-10 22:34:03 +0900

    Add Google Analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,9 +12,7 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: { provider: 'google', tagId: 'G-FKS35GN7DG' },
     locale: "en-US",
     baseUrl: "obsidian.kaedesato.work",
     ignorePatterns: ["private", "templates", ".obsidian"],
