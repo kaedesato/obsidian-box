1a64c328d8d0cd435169c5a4e9246563a9bf2705
Kaede Sato <214kaedesato@gmail.com>
2025-04-24 12:11:50 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -55,7 +55,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority:["git", "filesystem"],
+        priority:["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
